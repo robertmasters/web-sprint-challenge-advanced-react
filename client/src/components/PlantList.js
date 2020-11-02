854f98e4d08fda6ba746832fbbd59a6515b45bdf
Robert-Rodriguez <robertmasters@hotmail.com>
2020-11-01 16:15:52 -0800

    finished creating the custom hook

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -16,26 +16,28 @@ state = {
 }
 
 
-
-//   // when the component mounts:
-  componentDidMount(){
-    this.fetchPlantData();
-  }
+// when the component mounts:
+componentDidMount(){
+  this.fetchPlantData();
+  console.log("componentdidmount: ", this.state.plants)
+}
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   fetchPlantData = () => {
     fetch('http://localhost:3333/plants')
     .then((response)=> response.json())
     .then((response)=> {
-      //   - set the returned plants array to this.state.plants
+      //  - set the returned plants array to this.state.plants
       // console.log("pre: ",this.state.plants)
-      this.state.plants = response.plantsData
-      console.log("Plants Array: ", this.state.plants);
+      // this.state.plants = response.plantsData
+      // console.log("Plants Array: ", this.state.plants);
+
+      this.setState({
+        plants: response.plantsData
+      })
     })
     .catch((error)=>{
       console.log("error: ", error);
     })
-    
-
   }
 
       // .then((response)=>{
@@ -50,8 +52,9 @@ state = {
   render() {
     return (
       <main className="plant-list">
+      {console.log("render: ", this.state.plants)}
         {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id}>
+          <div data-testid="plant-card" className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
               <h2 className="plant-name">{plant.name}</h2>
